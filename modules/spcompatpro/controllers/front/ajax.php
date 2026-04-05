5ac27612eaf4ffae8abeec916147ef715864a86b
ecoplanetway-dev <ecoplanetway@gmail.com>
2026-04-05 20:15:39 +0200

    Create ajax.php with searchProduct, searchPsProduct, and getModelSpares methods for AJAX product search functionality

---
+++ modules/spcompatpro/controllers/front/ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+class AjaxController extends FrontController {
+
+    public function searchProduct($query) {
+        // Implementation of product search
+    }
+
+    public function searchPsProduct($query) {
+        // Implementation of product search in PS
+    }
+
+    public function getModelSpares($model) {
+        // Implementation of getting model spares
+    }
+}
+
+?>
